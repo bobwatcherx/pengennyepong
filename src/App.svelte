4377c086b90fe68963dab1f3d6dd23511f9ec7fd
unknown <bobwatcherx@gmail.com>
2024-04-06 14:52:11 +0700

    321

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import { Router,Route } from "svelte-navigator";
   import Home from "./pages/Home.svelte";
   import Player from "./pages/Player.svelte";
+  import Jav from "./pages/Jav.svelte";
 </script>
 
 <Router>
@@ -11,6 +12,9 @@
     </Route>
     <Route path="player/:id" let:params>
       <Player id={params.id} />
+    </Route>
+    <Route path="jav/:id" let:params>
+      <Jav javid={params.id} />
     </Route>
   </div>
 </Router>
